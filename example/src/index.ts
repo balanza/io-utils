6ee7d79e227704db458520611ae2b779f5a31b55
Emanuele De Cupis <ema.decu@gmail.com>
2020-04-20 20:09:27 +0200

    fix

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,14 +1,14 @@
-import { generateApi } from '@pagopa/io-utils';
+import { generateApi } from "@pagopa/io-utils";
 
 generateApi({
-    specFilePath: '../api.yaml',
-    definitionsDirPath: `${__dirname}/generated`,
-    strictInterfaces: true,
-    generateRequestTypes: true,
-    generateResponseDecoders: true,
-    generateClient: true,
-    defaultSuccessType: 'undefined',
-    defaultErrorType: 'Error'
+  specFilePath: `${process.cwd()}/api.yaml`,
+  definitionsDirPath: `${process.cwd()}/generated`,
+  strictInterfaces: true,
+  generateRequestTypes: true,
+  generateResponseDecoders: true,
+  generateClient: true,
+  defaultSuccessType: "undefined",
+  defaultErrorType: "Error"
 })
-.then(() => console.log('generation completed'))
-.catch((reason: any) => console.error('generation error', reason));
+  .then(() => console.log("generation completed"))
+  .catch((reason: any) => console.error("generation error", reason));
